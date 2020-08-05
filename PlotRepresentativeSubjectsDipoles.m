c3fbaee761520280bf70c5be18589af0dd8acea0
Abolfazl-Alipour <44123724+Abolfazl-Alipour@users.noreply.github.com>
2020-08-05 00:11:58 +0000

    Update PlotRepresentativeSubjectsDipoles.m

--- PlotRepresentativeSubjectsDipoles.m
+++ PlotRepresentativeSubjectsDipoles.m
@@ -1,7 +1,7 @@
 clear
 eeglab;
 close all
-destinationPath='E:\Abolfazl\OtherProjs\PDPredictiveCoding\Data\restingStatesLastWave(hopefully)\DownlaodedDataset\ProcessedData\';
+destinationPath='PATH TO EEG OBJECTS';
 
 CntrlList=[908,8010,8060,893,909,900,914,910,891,892,902];
 CntrlListCumPFCComps={};
@@ -9,7 +9,7 @@ mm=1;
 for i=1:length(CntrlList)
     subjectID=CntrlList(2);
     load([destinationPath,'EEGObj_Component',int2str(subjectID),'.mat'],'EEG')
-    EEG = pop_dipfit_settings( EEG, 'hdmfile','C:\\Users\\aalipour\\Documents\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_vol.mat','coordformat','MNI','mrifile','C:\\Users\\aalipour\\Documents\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_mri.mat','chanfile','C:\\Users\\aalipour\\Documents\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\elec\\standard_1005.elc','chansel',[1:EEG.nbchan] );
+    EEG = pop_dipfit_settings( EEG, 'hdmfile','PATH TO\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_vol.mat','coordformat','MNI','mrifile','PATH TO\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_mri.mat','chanfile','PATH TO\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\elec\\standard_1005.elc','chansel',[1:EEG.nbchan] );
     EEG = pop_multifit(EEG, [1:size(EEG.icaweights,1)] ,'threshold',100,'dipplot','on','plotopt',{'normlen' 'on'});
     
     PFCComps={};
@@ -22,9 +22,9 @@ for i=1:length(CntrlList)
         end
 
     end
-    pop_dipplot( EEG, [1:size(EEG.icaweights,1)] ,'rvrange',[0 100] ,'mri','C:\\Users\\aalipour\\Documents\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_mri.mat','normlen','on');%,'projlines','on'
+    pop_dipplot( EEG, [1:size(EEG.icaweights,1)] ,'rvrange',[0 100] ,'mri','PATH TO\Documents\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_mri.mat','normlen','on');%,'projlines','on'
    
-    pop_dipplot( EEG, [cell2mat(PFCComps)] ,'rvrange',[0 100] ,'mri','C:\\Users\\aalipour\\Documents\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_mri.mat','normlen','on');
+    pop_dipplot( EEG, [cell2mat(PFCComps)] ,'rvrange',[0 100] ,'mri','PATH TO\Documents\\MATLAB\\eeglab2019_1\\plugins\\dipfit\\standard_BEM\\standard_mri.mat','normlen','on');
     CntrlListCumPFCComps{mm}=cell2mat(PFCComps);
     mm=mm+1;
 
